a32f281766313e3f0e9a4f6594df7efe1fa39223
Nithish Raj <nithishraj2106@gmail.com>
2026-07-18 22:17:26 +0530

    Update MAIN.C

--- DAY-1/MAIN.C
+++ DAY-1/MAIN.C
@@ -2,6 +2,7 @@
 
 int main(){
     //hello this is my first time practing
+//so im just trying to do a contribution in my github 
     printf("I like food \n");
     printf("\nit's really good");
     return 0;
